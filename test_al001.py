af58d1be7a8a84c785f01bd1848990131079daed
Sephirothbot <sephirothbot@163.com>
2020-09-23 22:58:41 +0800

    Initial commit

---
+++ test_al001.py
@@ -0,0 +1,14 @@
+import pytest,allure
+
+class Test_lll:
+    
+    @allure.step(title="first test.")
+    # @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    def test_001(self):
+        allure.attach("This is .txt file","contain")
+    # @pytest.issue('http://www.163.com')
+    # @pytest.allure.testcase('http://www.baidu.com/test_al001')
+
+    def test_002(self):
+        allure.attach("This is .txt file","contain")
+        assert 0
